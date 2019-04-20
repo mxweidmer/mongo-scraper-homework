9dcd0158f6f523f5826a4e3a7f19bc582f8d2b65
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-04-19 20:25:31 -0700

    express server setup

--- server.js
+++ server.js
@@ -5,3 +5,17 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+})
