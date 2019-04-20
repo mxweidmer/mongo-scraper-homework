ebe8f2e4216ce5ab1e7a739a8eea222a0820ad8c
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-04-19 20:26:56 -0700

    mongodb database connection setup

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
 
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
