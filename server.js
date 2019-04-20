152cbb15a27cb1f429feec8f85ac269cde98007c
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-04-19 22:46:29 -0700

    created scraping route

--- server.js
+++ server.js
@@ -21,6 +21,30 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
+app.get("/scrape", function (req, res) {
+    axios.get("https://www.bonappetit.com/recipes").then(function (response) {
+        var $ = cheerio.load(response.data);
+
+        $("li.cards__li").each(function (i, element) {
+            var result = {};
+
+            result.headline = $(element).find(".card-hed").children("a").text();
+            result.summary = $(element).find("p.card-copy").text();
+            result.url = "https://www.bonappetit.com/" + $(element).find(".card-hed").children("a").attr("href");
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+
+        res.send("Scrape done");
+    });
+})
+
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
 })
